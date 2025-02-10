56a244529ca155ce4244dec4d738e7df055269be
shankeleven <shashank110702@gmail.com>
2025-02-11 00:33:52 +0530

    lsp configured

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -63,6 +63,9 @@ autocmd('LspAttach', {
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "ds", function() vim.lsp.buf.document_symbols() end, opts)
+        -- vim.keymap.set("n", "gi", function() vim.ls    ~/Documents/goback                                                                             p.buf._ () end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
