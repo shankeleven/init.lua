1bb3a028cbf8f22ad101c71333758da1870ea142
shankeleven <shashank110702@gmail.com>
2025-02-10 23:17:14 +0530

    harpoon configured

--- lua/theprimeagen/lazy/local.lua
+++ lua/theprimeagen/lazy/local.lua
@@ -16,14 +16,14 @@ local local_plugins = {
             vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
             vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-            vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
-            vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-            vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
-            vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
-            vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
+            vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<leader><C-1>", function() harpoon:list():replace_at(1) end)
+            vim.keymap.set("n", "<leader><C-2>", function() harpoon:list():replace_at(2) end)
+            vim.keymap.set("n", "<leader><C-3>", function() harpoon:list():replace_at(3) end)
+            vim.keymap.set("n", "<leader><C-4>", function() harpoon:list():replace_at(4) end)
         end
     },
 }
