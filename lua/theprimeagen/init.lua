a73aa42179db9aacce7e00eb3d8754aafe2bfa69
shankeleven <shashanksati11@gmail.com>
2025-07-25 04:16:52 +0530

    leader d for diagnostics and leader n/N for navigation
    Signed-off-by: shankeleven <shashanksati11@gmail.com>

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -69,13 +69,13 @@ autocmd('LspAttach', {
         -- vim.keymap.set("n", "gi", function() vim.ls    ~/Documents/goback                                                                             p.buf._ () end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
         vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "<leader>n", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "<leader>N", function() vim.diagnostic.goto_prev() end, opts)
         vim.keymap.set("n", "<C-h>", "zH", { noremap = true, silent = true }) -- horizontal scroll
         vim.keymap.set("n", "<C-l>", "zL", { noremap = true, silent = true })
 
