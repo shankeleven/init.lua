5a74d53898b6106973efa3cc69162614ff901962
shankeleven <shashanksati11@gmail.com>
2025-07-25 05:06:36 +0530

    essentials updated
    Signed-off-by: shankeleven <shashanksati11@gmail.com>

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -59,20 +59,21 @@ autocmd('BufEnter', {
 })
 
 
+-- essentials
 autocmd('LspAttach', {
     group = ThePrimeagenGroup,
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "ds", function() vim.lsp.buf.document_symbols() end, opts)
+        vim.keymap.set("n", "ds", function() vim.lsp.buf.document_symbol() end, opts)
         -- vim.keymap.set("n", "gi", function() vim.ls    ~/Documents/goback                                                                             p.buf._ () end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>w", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>c", function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set("n", "<leader>n", function() vim.diagnostic.goto_next() end, opts)
         vim.keymap.set("n", "<leader>N", function() vim.diagnostic.goto_prev() end, opts)
